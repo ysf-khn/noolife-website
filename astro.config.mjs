4d29b73a1e31613725d15ad2d4fb6f9bf1fa1c46
Yusuf <yusufmohd72@gmail.com>
2025-11-07 16:17:06 +0530

    add trailing slashes fix for gsc

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://noolife.app',
+  trailingSlash: 'always',
   integrations: [sitemap()],
   image: {
     remotePatterns: [
